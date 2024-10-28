9765d42973fe244e6ec16fee90720ae03481601e
Keith Gardner <keithgardner@Keiths-MacBook-Air.local>
2024-10-27 20:18:30 -0400

    Init

--- App.tsx
+++ App.tsx
@@ -25,6 +25,8 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
+import { RNCamera } from 'react-native-camera';
+
 type SectionProps = PropsWithChildren<{
   title: string;
 }>;
